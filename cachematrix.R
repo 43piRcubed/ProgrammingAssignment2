e8d4416156f4f76a97353856ec1b90217b9e8030
Frank Albrecht <frankAlbrecht@Franks-MBP.attlocal.net>
2015-06-13 13:50:30 -0700

    Solve problem with not getting cached results

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,32 @@
 ## caching the inverse of a matrix rather than compute it repeatedly
 
 ## creates a special "matrix" object that can cache its inverse
-inverse_matrix <- NULL
+
 makeCacheMatrix <- function(x = matrix()) {
-##   Function to make Matrix
-     makeMatrix <- function(x = matrix()) {
-          
-          set <- function(y) {
-               x <<- y
-               inverse_matrix <<- NULL
-          }
-          get <- function() x
-          setmatrix <- function(mat) inverse_matrix <<- mat
-          getmatrix <- function() inverse_matrix
-          list(set = set, get = get,
-               setmatrix = setmatrix,
-               getmatrix = getmatrix)
-     }
-##   Function to cache the inverse matrix     
-     cacheInvMatrix <- function(x, ...) {
-          inverse_matrix <- x$getmatrix()
-          print(inverse_matrix)
-          if(!is.null(inverse_matrix)) {
-               message("getting cached data")
-               return(inverse_matrix)
-          }
-          matrix <- x$get()
-          inverse_matrix <- cacheSolve(matrix)
-          x$setmatrix(inverse_matrix)
-          inverse_matrix
-     }
-     cacheInvMatrix(makeMatrix(x))
+     inverse_matrix <- NULL
+     get <- function() x
+     setmatrix <- function(mat) inverse_matrix <<- mat
+     getmatrix <- function() inverse_matrix
+     list(set = set, get = get,
+          setmatrix = setmatrix,
+          getmatrix = getmatrix)
 }
 
 
-## computes the inverse of the special "matrix" returned by makeCacheMatrix
+## computes the inverse of the special "matrix" returned by makeCacheMatrix and caches it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-     solve(x)
+     inverse_matrix <- x$getmatrix()
+     
+     ## Check if inverse exists. if exists return cahced result
+     if(!is.null(inverse_matrix)) {
+          message("getting cached data")
+          return(inverse_matrix)
+     }
+     
+     ## if inverse does not exist, compute the inverse and cache it
+     matrix <- x$get()
+     inverse_matrix <- solve(matrix)
+     x$setmatrix(inverse_matrix)
+     inverse_matrix
 }
